84ac98481d57bb619546391ff7abcc070c58b154
Alex Sharp <ajsharp@gmail.com>
2012-01-08 18:09:22 -0800

    Add em-stathat to test env

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source :rubygems
 
 group :test do
+  gem 'em-stathat'
   gem 'rack'
   gem 'rspec', '>= 2.7'
   gem 'rack-test', :require => 'rack/test'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,6 +3,12 @@ $:.push(File.dirname(File.expand_path(__FILE__)))
 require File.join(File.expand_path(File.dirname(__FILE__)), '..', 'lib', 'rack-request_profiler')
 
 require 'rack/test'
+require 'em-stathat'
+
+EventMachine::StatHat.config do |c|
+  c.ukey  = 'key'
+  c.email = 'user@example.com'
+end
 
 RSpec.configure do |config|
 end
