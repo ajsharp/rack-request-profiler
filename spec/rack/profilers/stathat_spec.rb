7b8c309113e4efd0e8b0b7a21cbfd2c615a77d1b
Alex Sharp <ajsharp@gmail.com>
2012-02-09 17:56:06 -0800

    Really ugly way to test Stathat profiler callbacks

--- spec/rack/profilers/stathat_spec.rb
+++ spec/rack/profilers/stathat_spec.rb
@@ -4,11 +4,16 @@ describe Rack::Profilers::Stathat do
   it "accepts callbacks" do
     EM.run {
       app = Rack::Builder.app do
-        use Rack::Profilers::Stathat, :callback => lambda { |req| puts("hey"); EM.stop }
+        use Rack::Profilers::Stathat, :callback => lambda { |req| }
 
         run lambda { |env| [200, {'Content-Type' => 'text/plain'}, ['']]}
       end
-      req = Rack::MockRequest.new(app).get('/')
+
+      deferrable = mock("Deferrable")
+      deferrable.expects(:callback).returns(EM.stop)
+      EM::StatHat.any_instance.expects(:ez_value).returns(deferrable)
+
+      Rack::MockRequest.new(app).get('/zomg/this/is/a/test')
     }
   end
 end
