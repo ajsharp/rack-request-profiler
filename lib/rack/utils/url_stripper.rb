f789e272fd106df1583a05536fe2befa27147fb4
Alex Sharp <ajsharp@gmail.com>
2012-07-17 07:45:06 -0700

    Fix #2. Handle BSON id's that start with more than one number

--- lib/rack/utils/url_stripper.rb
+++ lib/rack/utils/url_stripper.rb
@@ -1,7 +1,8 @@
 module Rack
   module Utils
     class UrlStripper
-      ID_PATTERN = /([0-9][a-z][^\/]+)/
+      # A crude attempt at matching a BSON id format (http://bsonspec.org/)
+      ID_PATTERN = /[0-9]+[a-zA-Z]+[^\/]*/
 
       def self.replace_id(url)
         url.gsub(ID_PATTERN, 'ID')
--- spec/rack/utils/url_stripper_spec.rb
+++ spec/rack/utils/url_stripper_spec.rb
@@ -3,7 +3,10 @@ require 'spec_helper'
 describe Rack::Utils::UrlStripper do
   describe '.replace_id' do
     it 'replaces BSON-like ids with ID' do
-      Rack::Utils::UrlStripper.replace_id("/resource/4f07931e3641417a88000002").should == '/resource/ID'
+      ids = %w(5005536b28330e5a8800005f 4f07931e3641417a88000002)
+      ids.each do |id|
+        Rack::Utils::UrlStripper.replace_id("/resource/#{id}").should == '/resource/ID'
+      end
     end
 
     it "replaces ids with sub-resources" do
